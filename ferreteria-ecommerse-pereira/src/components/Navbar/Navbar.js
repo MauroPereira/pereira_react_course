35e07364fac7390834a838acdb22d05bbe926761
MauroPereira <mauro.a.pereira@gmail.com>
2023-02-18 15:48:33 -0300

    todo: 14:06

---
+++ ferreteria-ecommerse-pereira/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+export const Navbar = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <h1 className="header__logo">LOGO</h1>
+        <nav>
+          <a href="#" className="navbar__link">
+            Link1
+          </a>
+          <a href="#" className="navbar__link">
+            Link2
+          </a>
+          <a href="#" className="navbar__link">
+            Link3
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
