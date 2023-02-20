764b14d4867c870e086841b03c8ff5b36f5b6745
MauroPereira <mauro.a.pereira@gmail.com>
2023-02-20 16:01:55 -0300

    done: agregado categorias cliqueables

--- ferreteria-ecommerse-pereira/src/components/Categories/Categories.js
+++ ferreteria-ecommerse-pereira/src/components/Categories/Categories.js
@@ -1,11 +1,34 @@
 export const Categories = () => {
   return (
-    <form>
-    <label>
-      Name:
-      <input type="text" name="name" />
-    </label>
-    <input type="submit" value="Submit" />
-  </form>
+  <section className="section">
+      <div className = "section__container">
+        <h1 className="form__title">Categorías de rubros</h1>
+
+        <form className="form__categories">          
+          <label className="form__label">
+            Herramientas
+            <input type="checkbox" name="herramientas" />             
+          </label>
+
+          <label className="form__label">
+            Electricidad
+            <input type="checkbox" name="electricidad" />             
+          </label>          
+
+          <label className="form__label">
+            Construcción
+            <input type="checkbox" name="contruccion" />             
+          </label>
+
+          <label className="form__label">
+            Muebles
+            <input type="checkbox" name="muebles" />             
+          </label>
+          
+          <input type="submit" value="Seleccionar"></input>
+          
+        </form>      
+      </div> 
+    </section>   
   );
 };
