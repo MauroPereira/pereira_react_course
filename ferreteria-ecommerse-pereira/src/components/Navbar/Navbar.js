ce0838cbce08d12ac90039c5d2ef6d0eadf42abc
MauroPereira <mauro.a.pereira@gmail.com>
2023-02-20 23:29:07 -0300

    done: icono con ruta relativa desde index.html

--- ferreteria-ecommerse-pereira/src/components/Navbar/Navbar.js
+++ ferreteria-ecommerse-pereira/src/components/Navbar/Navbar.js
@@ -4,16 +4,18 @@ export const Navbar = () => {
   return (
     <header className="header">
       <div className="header__container">
-        <h1 className="header__logo">LOGO</h1>
+        {/* <h1 className="header__logo">LOGO</h1> */}
+        <img src='./imgs/icono_ferreteria.png' className='header__logo' alt='logo'/>
+
         <nav className="navbar">
           <a href="#" className="navbar__link">
-            Link1
+            Proveedores oficiales
           </a>
           <a href="#" className="navbar__link">
-            Link2
+            ¿Quiénes somos?
           </a>
           <a href="#" className="navbar__link">
-            Link3
+            Contáctanos
           </a>
         </nav>
       </div>
