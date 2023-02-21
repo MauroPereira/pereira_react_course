cd06fd13dcc7690b06669f6ef26a0205f8dc0350
MauroPereira <mauro.a.pereira@gmail.com>
2023-02-21 13:08:19 -0300

    done: implementado contador hardcodeado

--- ferreteria-ecommerse-pereira/src/components/CartWidget/CartWidget.js
+++ ferreteria-ecommerse-pereira/src/components/CartWidget/CartWidget.js
@@ -5,6 +5,9 @@ export const CartWidget = () => {
     return (
         <div className='cartwidget__container'>
             <img src={iconCarrito} alt='changuito'/>
+            <div className='number__container'>
+                <label className='numbercontainer__number'>0</label>
+            </div>
         </div>
     );
 }
