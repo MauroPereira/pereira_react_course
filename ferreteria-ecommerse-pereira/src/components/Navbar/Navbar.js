cde1f895719e4d2ef361efcc3529fcd436342592
MauroPereira <mauro.a.pereira@gmail.com>
2023-02-20 23:32:07 -0300

    done: logo como modulo importado dentro de las carpetas de components

--- ferreteria-ecommerse-pereira/src/components/Navbar/Navbar.js
+++ ferreteria-ecommerse-pereira/src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import './Navbar.scss'
+import logo from './icono_ferreteria.png'
 
 export const Navbar = () => {
   return (
     <header className="header">
       <div className="header__container">
         {/* <h1 className="header__logo">LOGO</h1> */}
-        <img src='./imgs/icono_ferreteria.png' className='header__logo' alt='logo'/>
+        <img src={logo} className='header__logo' alt='logo'/>
 
         <nav className="navbar">
           <a href="#" className="navbar__link">
