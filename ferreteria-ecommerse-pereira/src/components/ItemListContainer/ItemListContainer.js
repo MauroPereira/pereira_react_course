ae3df7e939b0b9776f003eea7c6788c8cf3de0eb
MauroPereira <mauro.a.pereira@gmail.com>
2023-03-12 16:44:31 -0300

    commit anterior

--- ferreteria-ecommerse-pereira/src/components/ItemListContainer/ItemListContainer.js
+++ ferreteria-ecommerse-pereira/src/components/ItemListContainer/ItemListContainer.js
@@ -7,12 +7,29 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/Inbox";
 import DraftsIcon from "@mui/icons-material/Drafts";
+import { useEffect } from "react";
+import { dataRequest } from "../../helpers/dataRequest";
 
 const MOCK_DATA = [];
 
 export const ItemListContainer = ({ greeting }) => {
   const [products, setProducts] = useState([]);
-  const [loadingMsg, setLoadingMsg] = useState(false); // Primero False para no mostrarlo
+  const [loadingMsg, setLoadingMsg] = useState(false); // Primero False para no mostrar el mensaje de Loading
+
+  // Para que no se vuelva a cargar //////////
+  useEffect(() => {
+    dataRequest()
+      .then((response) => {
+        setProducts(response);
+      })
+      .catch((error) => {
+        console.log(error); // se imprime si ha ocurrido un error
+      })
+      .finally(() => {
+        setLoadingMsg(false); // para asegurarse que no se muestre el mensaje de Loading
+      });
+  }, []);
+  ///////////////////////////////////////////
 
   return (
     <div className="itemlist__container">
@@ -28,31 +45,33 @@ export const ItemListContainer = ({ greeting }) => {
   );
 };
 
-export const ItemListContainerMui = () => {
-  return (
-    <div className="itemlistmui__container">
-      <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        <nav aria-label="main mailbox folders">
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary="Inbox" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <DraftsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Drafts" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </nav>
-      </Box>
-    </div>
-  );
-};
+export default ItemListContainer;
+
+// export const ItemListContainerMui = () => {
+//   return (
+//     <div className="itemlistmui__container">
+//       <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+//         <nav aria-label="main mailbox folders">
+//           <List>
+//             <ListItem disablePadding>
+//               <ListItemButton>
+//                 <ListItemIcon>
+//                   <InboxIcon />
+//                 </ListItemIcon>
+//                 <ListItemText primary="Inbox" />
+//               </ListItemButton>
+//             </ListItem>
+//             <ListItem disablePadding>
+//               <ListItemButton>
+//                 <ListItemIcon>
+//                   <DraftsIcon />
+//                 </ListItemIcon>
+//                 <ListItemText primary="Drafts" />
+//               </ListItemButton>
+//             </ListItem>
+//           </List>
+//         </nav>
+//       </Box>
+//     </div>
+//   );
+// };
