3f1bffd06a4b6c35573e0f0ab916659c1bad291e
MauroPereira <mauro.a.pereira@gmail.com>
2023-03-12 23:17:54 -0300

    bugfixed: el tamano de la imagen del cable

--- ferreteria-ecommerse-pereira/src/components/Item/Item.js
+++ ferreteria-ecommerse-pereira/src/components/Item/Item.js
@@ -1,18 +1,22 @@
+import "./Item.scss";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 const Item = ({ item }) => {
   return (
-    <div className="col-3 m-1">
-      <img src={item.img} alt={item.name} />
-      <h4>{item.name}</h4>
-      <p>{item.description}</p>
-      <p>
-        Precio: <strong>${item.price}</strong>
-      </p>
-      <p>Cantidad: {item.stock}</p>
-      <Button type="submit" variant="contained">
-        Agregar al Carrito de compras
-      </Button>
+    <div>
+      <Grid item xs={12}>
+        <img src={item.img} alt={item.name} />
+        <h4>{item.name}</h4>
+        <p className="itemDescription">{item.description}</p>
+        <p>
+          Precio: <strong>${item.price}</strong>
+        </p>
+        <p>Cantidad: {item.stock}</p>
+        <Button type="submit" variant="contained">
+          Agregar al Carrito de compras
+        </Button>
+      </Grid>
     </div>
   );
 };
--- ferreteria-ecommerse-pereira/src/components/ItemList/ItemList.js
+++ ferreteria-ecommerse-pereira/src/components/ItemList/ItemList.js
@@ -1,11 +1,14 @@
 import Item from "../Item/Item";
+import Grid from "@mui/material/Grid";
 
 const ItemList = ({ items }) => {
   return (
-    <div className="row my-5">
-      {items.map((product) => (
-        <Item key={product.id} item={product} />
-      ))}
+    <div>
+      <Grid container spacing={2}>
+        {items.map((product) => (
+          <Item key={product.id} item={product} />
+        ))}
+      </Grid>
     </div>
   );
 };
