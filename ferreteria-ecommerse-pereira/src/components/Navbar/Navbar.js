471a6d2c38d0fb19494d2d33cda35cc03fd20dc1
MauroPereira <mauro.a.pereira@gmail.com>
2023-03-13 18:31:48 -0300

    feat: destinos a enlaces de navbar

--- ferreteria-ecommerse-pereira/src/components/Navbar/Navbar.js
+++ ferreteria-ecommerse-pereira/src/components/Navbar/Navbar.js
@@ -1,28 +1,27 @@
-import './Navbar.scss'
-import logo from './icono_ferreteria.png'
-import { CartWidget } from '../CartWidget/CartWidget';
+import "./Navbar.scss";
+import logo from "./icono_ferreteria.png";
+import { CartWidget } from "../CartWidget/CartWidget";
 
 export const Navbar = () => {
   return (
     <header className="header">
       <div className="header__container">
         {/* <h1 className="header__logo">LOGO</h1> */}
-        <img src={logo} className='header__logo' alt='logo'/>
+        <img src={logo} className="header__logo" alt="logo" />
 
         <nav className="navbar">
-          <a href="#" className="navbar__link">
-            Proveedores oficiales
+          <a href="/" className="navbar__link">
+            Inicio
           </a>
-          <a href="#" className="navbar__link">
-            ¿Quiénes somos?
+          <a href="/nosotros" className="navbar__link">
+            Nosotros
           </a>
-          <a href="#" className="navbar__link">
-            Contáctanos
+          <a href="/contacto" className="navbar__link">
+            Contacto
           </a>
         </nav>
 
-        <CartWidget/>
-        
+        <CartWidget />
       </div>
     </header>
   );
