695e002ea2bf31d482b86baffc6ddb3a9b3232ce
MauroPereira <mauro.a.pereira@gmail.com>
2023-03-13 00:34:32 -0300

    done

--- ferreteria-ecommerse-pereira/src/components/Item/Item.js
+++ ferreteria-ecommerse-pereira/src/components/Item/Item.js
@@ -4,7 +4,7 @@ import Grid from "@mui/material/Grid";
 
 const Item = ({ item }) => {
   return (
-    <Grid item xs={4}>
+    <Grid className="product_item" item xs={3}>
       <img src={item.img} alt={item.name} />
       <h4>{item.name}</h4>
       <p className="itemDescription">{item.description}</p>
