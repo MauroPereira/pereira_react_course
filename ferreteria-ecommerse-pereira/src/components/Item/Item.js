48818fda6e945fdc850f651aad486a0faafd3b2f
MauroPereira <mauro.a.pereira@gmail.com>
2023-03-18 23:16:33 -0300

    todo: poner mas bloques

--- ferreteria-ecommerse-pereira/src/components/Item/Item.js
+++ ferreteria-ecommerse-pereira/src/components/Item/Item.js
@@ -9,21 +9,19 @@ import Paper from "@mui/material/Paper";
 export const Item = ({ item }) => {
   return (
     <Paper className="paper_item" elevation={6}>
-      <Box spacing={2}>
-        <Grid className="product_item" item xs={3}>
-          <img src={item.img} alt={item.name} />
-          <h4>{item.name}</h4>
-          <p className="itemDescription">{item.description}</p>
-          <p>
-            Precio: <strong>${item.price}</strong>
-          </p>
-          <p>Cantidad: {item.stock}</p>
-          <Button variant="contained">
-            <Link to={`/detail/${item.id}`} style={{ textDecoration: "none" }}>
-              Ver más
-            </Link>
-          </Button>
-        </Grid>
+      <Box className="product_item">
+        <img src={item.img} alt={item.name} />
+        <h4>{item.name}</h4>
+        <p className="itemDescription">{item.description}</p>
+        <p>
+          Precio: <strong>${item.price}</strong>
+        </p>
+        <p>Cantidad: {item.stock}</p>
+        <Button variant="contained">
+          <Link to={`/detail/${item.id}`} style={{ textDecoration: "none" }}>
+            Ver más
+          </Link>
+        </Button>
       </Box>
     </Paper>
   );
