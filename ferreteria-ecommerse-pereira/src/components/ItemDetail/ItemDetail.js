946f484b9b89eb6ea485d032558beba28318c5a1
MauroPereira <mauro.a.pereira@gmail.com>
2023-03-21 07:49:47 -0300

    done(ItemDetail.js)

--- ferreteria-ecommerse-pereira/src/components/ItemDetail/ItemDetail.js
+++ ferreteria-ecommerse-pereira/src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,8 @@
 import "./ItemDetail.scss";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 
 const ItemDetail = ({ item }) => {
   const navigate = useNavigate();
@@ -10,17 +12,21 @@ const ItemDetail = ({ item }) => {
   };
 
   return (
-    <div className="item_detail">
-      <img src={item.img} alt={item.name} />
-      <h4>{item.name}</h4>
-      <p>{item.description}</p>
-      <p>
-        Precio: <strong>${item.price}</strong>
-      </p>
-      <p>Cantidad: {item.stock}</p>
-      <Button variant="contained" onClick={handleReturn}>
-        Volver
-      </Button>
+    <div className="item_detail__container">
+      <Paper className="paper_item" elevation={6}>
+        <Box className="box_item">
+          <img src={item.img} alt={item.name} />
+          <h4 className="itemName">{item.name}</h4>
+          <p className="itemDescription">{item.description}</p>
+          <p className="itemPrice">
+            Precio: <strong>${item.price}</strong>
+          </p>
+          <p className="itemQuantity">Cantidad: {item.stock}</p>
+          <Button variant="contained" onClick={handleReturn}>
+            Volver
+          </Button>
+        </Box>
+      </Paper>
     </div>
   );
 };
