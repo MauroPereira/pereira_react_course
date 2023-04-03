3d487696ad5e22568be5ae014939375df8fae399
MauroPereira <mauro.a.pereira@gmail.com>
2023-04-03 10:30:51 -0300

    bugfixed: corregido los enlaces a imagenes. todo: sa2

--- ferreteria-ecommerse-pereira/src/components/AddToCart/AddToCart.js
+++ ferreteria-ecommerse-pereira/src/components/AddToCart/AddToCart.js
@@ -1,3 +1,21 @@
+// import Swal from "sweetalert2";
+
+// const successAlert = () => {
+//   Swal.fire({  
+//       title: 'Good job!',  
+//       text: 'You clicked the button.',
+//       icon: 'success'
+//     }); 
+// }
+
+// const questionAlert = () => {
+//   Swal.fire({  
+//       title: 'Do you have a problem to solve?!',  
+//       text: 'Ask us on dirask',
+//       icon: 'question'
+//     }); 
+// }
+
 export const addToCart = (itemToAdd, cart, setCart) => {
   const duplicatedItem = cart.find((obj) => obj.id === itemToAdd.id);
 
@@ -8,19 +26,26 @@ export const addToCart = (itemToAdd, cart, setCart) => {
 
   if (duplicatedItem === undefined) {
     setCart([...cart, itemToAdd]);
+    console.log("Item agregado");
+    // successAlert();
 
     // const MySwal = withReactContent(Swal);
+    
     // MySwal.fire({
     //   title: <strong>Agregado el item</strong>,
     //   html: <i>You clicked the button!</i>,
     //   icon: "success",
     // });
+
   } else {
+    console.log("Item duplicado, no agregado");
     // const MySwal = withReactContent(Swal);
+ 
     // MySwal.fire({
     //   title: <strong>Item repetido</strong>,
     //   html: <i>You clicked the button!</i>,
     //   icon: "error",
     // });
+    // questionAlert();
   }
 };
