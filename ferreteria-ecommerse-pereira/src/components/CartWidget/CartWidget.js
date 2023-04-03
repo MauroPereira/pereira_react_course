cf749f046b711e42ec73e244b281b984cd683ef3
MauroPereira <mauro.a.pereira@gmail.com>
2023-04-03 17:51:40 -0300

    feat(CartWidget.js): agregado contador interactivo en el carrito

--- ferreteria-ecommerse-pereira/src/components/CartWidget/CartWidget.js
+++ ferreteria-ecommerse-pereira/src/components/CartWidget/CartWidget.js
@@ -1,12 +1,17 @@
 import './CartWidget.scss'
 import iconCarrito from './changuito.png'
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
 
 export const CartWidget = () => {
+
+    const { cart } = useContext(CartContext);
+
     return (
         <div className='cartwidget__container'>
             <img src={iconCarrito} alt='changuito'/>
             <div className='number__container'>
-                <label className='numbercontainer__number'>0</label>
+                <label className='numbercontainer__number'>{cart.length}</label>
             </div>
         </div>
     );
