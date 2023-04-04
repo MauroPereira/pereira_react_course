590ab77aed2e67f1e4be6364b85f8834702af312
MauroPereira <mauro.a.pereira@gmail.com>
2023-04-03 21:56:35 -0300

    feat(Cart.js): creado el esqueleto de todo el carrito. todo: estética

--- ferreteria-ecommerse-pereira/src/components/Cart/Cart.js
+++ ferreteria-ecommerse-pereira/src/components/Cart/Cart.js
@@ -1,8 +1,50 @@
 import "./Cart.scss";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../context/CartContext";
+import Button from "@mui/material/Button";
+
 export const Cart = () => {
+  const { cart, totalPrice, eraseCart, eraseItemFromCart } =
+    useContext(CartContext);
+  const navigate = useNavigate();
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="cart__container">
-      <h2>Tu compra</h2>
+      <h2>Carrito de compras</h2>
+      <hr />
+      {cart.map((prod) => (
+        <div key={prod.id}>
+          <img src={prod.img} alt={prod.name} />
+          <big>Precio por unidad: ${prod.price}</big>
+          <big>Cantidad: {prod.quantity}</big>
+          <big>Subtotal: ${(prod.price * prod.quantity).toFixed(3)}</big>
+          <Button
+            className="volver__btn"
+            variant="contained"
+            onClick={() => eraseItemFromCart(prod.id)}
+          >
+            Eliminar producto
+          </Button>
+          <hr />
+        </div>
+      ))}
+      <h3>Total: ${totalPrice().toFixed(3)}</h3>
+
+      <Button className="volver__btn" variant="contained" onClick={eraseCart}>
+        Borrar carrito
+      </Button>
+
+      <Button
+        className="volver__btn"
+        variant="contained"
+        onClick={handleReturn}
+      >
+        Volver
+      </Button>
     </div>
   );
 };
--- ferreteria-ecommerse-pereira/src/context/CartContext.js
+++ ferreteria-ecommerse-pereira/src/context/CartContext.js
@@ -3,12 +3,33 @@ import { addToCart } from "../components/AddToCart/AddToCart";
 
 export const CartContext = createContext();
 
-export const CartProvider = ( {children}) => {
-    const [cart, setCart] = useState([]);
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
 
-    return (
-        <CartContext.Provider value={{ cart, setCart, addToCart }}> 
-            {children}
-        </CartContext.Provider>
-    )
-}
+  const totalPrice = () => {
+    return cart.reduce((acc, prod) => acc + prod.quantity * prod.price, 0);
+  };
+
+  const eraseCart = () => {
+    setCart([]);
+  };
+
+  const eraseItemFromCart = (id) => {
+    setCart(cart.filter((prod) => prod.id !== id));
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        setCart,
+        addToCart,
+        totalPrice,
+        eraseCart,
+        eraseItemFromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
