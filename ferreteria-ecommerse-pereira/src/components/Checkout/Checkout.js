5de7744877ec3413dc46660d822b969b327228e9
MauroPereira <mauro.a.pereira@gmail.com>
2023-04-05 22:12:21 -0300

    todo: ingresar formulario con formik

--- ferreteria-ecommerse-pereira/src/components/Checkout/Checkout.js
+++ ferreteria-ecommerse-pereira/src/components/Checkout/Checkout.js
@@ -2,16 +2,25 @@ import "./Checkout.scss";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useState } from "react";
 
 export const Checkout = () => {
   const navigate = useNavigate();
   const handleReturn = () => {
     navigate(-1);
   };
+
+  const [values, setValues] = useState({
+    nombre: "",
+    direccion: "",
+    email: "",
+  });
+
   return (
     <div className="checkout__container">
       <h2>Checkout</h2>
       <hr />
+      ////// Formulario /////////////////
       <Button
         className="volver__btn"
         variant="contained"
