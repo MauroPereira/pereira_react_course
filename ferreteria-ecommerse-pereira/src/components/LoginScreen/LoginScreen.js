e13cdcc1e6584f910d7e9f6c798e65c71edff4e1
MauroPereira <mauro.a.pereira@gmail.com>
2023-05-09 18:08:48 -0300

    done: video 25:45 forma engorrosa

--- ferreteria-ecommerse-pereira/src/components/LoginScreen/LoginScreen.js
+++ ferreteria-ecommerse-pereira/src/components/LoginScreen/LoginScreen.js
@@ -1,12 +1,49 @@
-import "./LoginScreen.scss"
+import { useState } from "react";
+import "./LoginScreen.scss";
 
 export const LoginScreen = () => {
-	return(
-		<div className='login_screen__container'>
-			<h2>Login</h2>
-			<hr/>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-		</div>
-	)
+  const handleOnChangeEmail = (e) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+
+  const handleOnChangePassword = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
+  return (
+    <div className="login_screen__container">
+      <h2>Login</h2>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <input
+          value={email}
+          type={"text"}
+          onChange={handleOnChangeEmail}
+          className="form-control"
+          placeholder="Tu email"
+        ></input>
+        <input
+          value={password}
+          type={"text"}
+          onChange={handleOnChangePassword}
+          className="form-control my-3"
+          placeholder="Tu password"
+        ></input>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
-
