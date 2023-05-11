7868372eb62cd668259be20401bd91517b474a9e
MauroPereira <mauro.a.pereira@gmail.com>
2023-05-10 22:21:08 -0300

    video: 01:06:32

--- ferreteria-ecommerse-pereira/src/App.js
+++ ferreteria-ecommerse-pereira/src/App.js
@@ -1,97 +1,19 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar/Navbar";
-import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-import { MainTitle } from "./components/MainTitle/MainTitle";
-import DolarsiApi from "./components/DolarsiApi/DolarsiApi";
-import AboutUs from "./components/AboutUs/AboutUs";
-import Contact from "./components/Contact/Contact";
-import { Cart } from "./components/Cart/Cart";
-import { Checkout } from "./components/Checkout/Checkout";
-import { LoadFile } from "./components/Loadfile/LoadFile";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { CartProvider } from "./context/CartContext";
 import { LoginProvider } from "./context/LoginContext";
+import { AppRouter } from "./router/AppRouter";
 
 function App() {
   return (
     <LoginProvider>
       <CartProvider>
         <BrowserRouter>
-          {user.logged ? (
-            <>
-              <Navbar />
-              <MainTitle />
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <div>
-                      <DolarsiApi />
-                      <ItemListContainer greeting="Productos" />
-                    </div>
-                  }
-                />
-                <Route
-                  path="/productos/:categoryId"
-                  element={
-                    <div>
-                      <DolarsiApi />
-                      <ItemListContainer greeting="Productos" />
-                    </div>
-                  }
-                />
-                <Route
-                  path="/detail/:itemId"
-                  element={
-                    <div>
-                      <DolarsiApi />
-                      <ItemDetailContainer greeting="Detalle de producto" />
-                    </div>
-                  }
-                />
-                <Route path="/nosotros" element={<AboutUs />} />
-                <Route path="/contacto" element={<Contact />} />
-                <Route
-                  path="/cart"
-                  element={
-                    <div>
-                      <DolarsiApi />
-                      <Cart />
-                    </div>
-                  }
-                />
-                <Route
-                  path="/checkout"
-                  element={
-                    <div>
-                      <DolarsiApi />
-                      <Checkout />
-                    </div>
-                  }
-                />
-                <Route
-                  path="/loadfile"
-                  element={
-                    <div>
-                      <LoadFile />
-                    </div>
-                  }
-                />
-                <Route path="*" element={<Navigate to="/" />} />
-              </Routes>
-            </>
-          ) : (
-            <Routes>
-              <Route path="/login" element={<LoginScreen />} />
-              <Route path="*" element={<LoginScreen />} />
-            </Routes>
-          )}
-
+          <AppRouter />
           {/* {<Footer />} */}
         </BrowserRouter>
       </CartProvider>
---
+++ ferreteria-ecommerse-pereira/src/router/AppRouter.js
@@ -0,0 +1,93 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Navbar } from "../components/Navbar/Navbar";
+import { ItemListContainer } from "../components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { MainTitle } from "../components/MainTitle/MainTitle";
+import DolarsiApi from "../components/DolarsiApi/DolarsiApi";
+import AboutUs from "../components/AboutUs/AboutUs";
+import Contact from "../components/Contact/Contact";
+import { Cart } from "../components/Cart/Cart";
+import { Checkout } from "../components/Checkout/Checkout";
+import { LoadFile } from "../components/Loadfile/LoadFile";
+import { useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
+
+export const AppRouter = () => {
+  const { user } = useContext(LoginContext);
+
+  return (
+    <BrowserRouter>
+      {user.logged ? (
+        <>
+          <Navbar />
+          <MainTitle />
+          {/* Rutas privadas */}
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div>
+                  <DolarsiApi />
+                  <ItemListContainer greeting="Productos" />
+                </div>
+              }
+            />
+            <Route
+              path="/productos/:categoryId"
+              element={
+                <div>
+                  <DolarsiApi />
+                  <ItemListContainer greeting="Productos" />
+                </div>
+              }
+            />
+            <Route
+              path="/detail/:itemId"
+              element={
+                <div>
+                  <DolarsiApi />
+                  <ItemDetailContainer greeting="Detalle de producto" />
+                </div>
+              }
+            />
+            <Route path="/nosotros" element={<AboutUs />} />
+            <Route path="/contacto" element={<Contact />} />
+            <Route
+              path="/cart"
+              element={
+                <div>
+                  <DolarsiApi />
+                  <Cart />
+                </div>
+              }
+            />
+            <Route
+              path="/checkout"
+              element={
+                <div>
+                  <DolarsiApi />
+                  <Checkout />
+                </div>
+              }
+            />
+            <Route
+              path="/loadfile"
+              element={
+                <div>
+                  <LoadFile />
+                </div>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </>
+      ) : (
+        <Routes>
+          {/* Rutas públicas */}
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="*" element={<LoginScreen />} />
+        </Routes>
+      )}
+    </BrowserRouter>
+  );
+};
