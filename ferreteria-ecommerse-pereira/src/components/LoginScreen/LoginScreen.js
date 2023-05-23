b3d60784c911ceba8f7f46892ffb074189bc7209
MauroPereira <mauro.a.pereira@gmail.com>
2023-05-22 21:47:03 -0300

    todo: copiar las tarjetas en login

--- ferreteria-ecommerse-pereira/src/components/LoginScreen/LoginScreen.js
+++ ferreteria-ecommerse-pereira/src/components/LoginScreen/LoginScreen.js
@@ -1,6 +1,15 @@
 import "./LoginScreen.scss";
 import { useState, useContext } from "react";
 import { LoginContext } from "../../context/LoginContext";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 export const LoginScreen = () => {
   const { tryLogin } = useContext(LoginContext);
@@ -25,30 +34,73 @@ export const LoginScreen = () => {
   };
 
   return (
-    <div className="login_screen__container">
-      <h2>Login</h2>
-      <hr />
-      <form onSubmit={handleSubmit}>
-        <input
-          value={values.email}
-          type={"text"}
-          onChange={handleInputChange}
-          className="form-control"
-          placeholder="Tu email"
-          name="email"
-        ></input>
-        <input
-          value={values.password}
-          type={"password"}
-          onChange={handleInputChange}
-          className="form-control my-3"
-          placeholder="Tu password"
-          name="password"
-        ></input>
-        <button className="btn btn-primary" type="submit">
-          Login
-        </button>
-      </form>
-    </div>
+    <Container className="login__container" component="main" maxWidth="sm">
+      <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 5,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            onChange={handleInputChange}
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            onChange={handleInputChange}
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 };
+
+// Font: https://frontendshape.com/post/react-mui-5-login-page-example
